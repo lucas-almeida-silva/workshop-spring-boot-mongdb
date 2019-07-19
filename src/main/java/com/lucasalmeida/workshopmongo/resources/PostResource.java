f0a4811eec205f1235032ebf0cf21d09cabfbab2
Lucas Almeida <lucas_almeidasilva01@hotmail.com>
2019-07-19 16:43:30 -0300

    Consulta simples com query methods

--- src/main/java/com/lucasalmeida/workshopmongo/resources/PostResource.java
+++ src/main/java/com/lucasalmeida/workshopmongo/resources/PostResource.java
@@ -1,14 +1,17 @@
 package com.lucasalmeida.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucasalmeida.workshopmongo.domain.Post;
-import com.lucasalmeida.workshopmongo.dto.UserDTO;
+import com.lucasalmeida.workshopmongo.resources.util.URL;
 import com.lucasalmeida.workshopmongo.services.PostService;
 
 @RestController //indica que a classe é um recurso rest
@@ -23,5 +26,12 @@ public class PostResource {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-
+	
+	//Endpoint para fazer a busca
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String text){
+		text = URL.decodeParam(text); //decodifica o texto
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
 }
--- src/main/java/com/lucasalmeida/workshopmongo/services/PostService.java
+++ src/main/java/com/lucasalmeida/workshopmongo/services/PostService.java
@@ -1,12 +1,12 @@
 package com.lucasalmeida.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucasalmeida.workshopmongo.domain.Post;
-import com.lucasalmeida.workshopmongo.domain.User;
 import com.lucasalmeida.workshopmongo.repository.PostRepository;
 import com.lucasalmeida.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -19,5 +19,9 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
