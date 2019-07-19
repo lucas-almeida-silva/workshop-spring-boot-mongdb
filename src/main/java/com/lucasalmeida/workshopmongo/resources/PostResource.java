ce1207ae29096408392b1d1ac0212121133ff5de
Lucas Almeida <lucas_almeidasilva01@hotmail.com>
2019-07-19 00:01:11 -0300

    Obtendo post por id

---
+++ src/main/java/com/lucasalmeida/workshopmongo/resources/PostResource.java
@@ -0,0 +1,27 @@
+package com.lucasalmeida.workshopmongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lucasalmeida.workshopmongo.domain.Post;
+import com.lucasalmeida.workshopmongo.dto.UserDTO;
+import com.lucasalmeida.workshopmongo.services.PostService;
+
+@RestController //indica que a classe é um recurso rest
+@RequestMapping(value="/posts") //caminho do endpoint
+public class PostResource {
+	
+	@Autowired
+	private PostService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id){ //PathVariable - para que o id case com o id recebido na url
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
---
+++ src/main/java/com/lucasalmeida/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.lucasalmeida.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucasalmeida.workshopmongo.domain.Post;
+import com.lucasalmeida.workshopmongo.domain.User;
+import com.lucasalmeida.workshopmongo.repository.PostRepository;
+import com.lucasalmeida.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired //notação para instanciar automaticamente um objeto no serviço, injeção de dependência automática
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
