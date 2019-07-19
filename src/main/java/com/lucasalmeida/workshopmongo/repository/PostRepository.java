8f34e17f45cd1ae96ce82421893ffea1c4621a4b
Lucas Almeida <lucas_almeidasilva01@hotmail.com>
2019-07-19 16:58:50 -0300

    Consulta simples com @Query

--- src/main/java/com/lucasalmeida/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/lucasalmeida/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.lucasalmeida.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.lucasalmeida.workshopmongo.domain.Post;
@@ -10,5 +11,8 @@ import com.lucasalmeida.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> { //<tipo da classe de domínio que será gerenciada, tipo do ID na classe>
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }") //?0 - primeiro parâmetro que vem no método, ?2 é o segudo, etc
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
